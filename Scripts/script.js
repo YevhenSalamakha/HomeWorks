a18a47096519f68c6ffce9390ae5c795ac6f41fb
Yevhenii <80651146+YevhenSalamakha@users.noreply.github.com>
2021-10-04 00:40:01 +0300

    1 and 2 exercise done

---
+++ Scripts/script.js
@@ -0,0 +1,13 @@
+"use strict";
+// const numberOfFilms = +prompt('Сколько фильмов уже посмотрели?', '');
+
+// let personalMovieDB = {
+// 	count: alert(numberOfFilms),
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false,
+// };
+
+const userQueastions = prompt('Один из просмотренных последних фильмов?', '');
+prompt('На сколько оцените его?', '');
